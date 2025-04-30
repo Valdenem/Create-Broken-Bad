7dffc07cae2092df1d9f89f89841e181df540394
Valdenem <108012732+Valdenem@users.noreply.github.com>
2025-04-30 16:11:41 +0200

    Made blue and white meth edible and apply the meth effect.

--- src/main/java/com/jetpacker06/CreateBrokenBad/item/MethItem.java
+++ src/main/java/com/jetpacker06/CreateBrokenBad/item/MethItem.java
@@ -2,12 +2,19 @@ package com.jetpacker06.CreateBrokenBad.item;
 
 import com.jetpacker06.CreateBrokenBad.block.TrayBlock;
 import com.jetpacker06.CreateBrokenBad.register.CBBBlocks;
+import com.jetpacker06.CreateBrokenBad.register.CBBEffects;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
 
@@ -33,13 +40,53 @@ public class MethItem extends Item {
         return InteractionResult.CONSUME;
     }
     public static class Blue extends MethItem {
+        private static final int MAX_LEVEL = 3;
+
         public Blue(Properties pProperties) {
-            super(pProperties);
+            super(pProperties.food(new FoodProperties.Builder()
+                    .alwaysEat()
+                    .nutrition(0)
+                    .saturationMod(0)
+                    .effect(() -> new MobEffectInstance(CBBEffects.METH.get(), 1200, 0), 1)
+                    .build()));
+        }
+        @Override
+        public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
+            if(!level.isClientSide && entity instanceof ServerPlayer player) {
+                MobEffectInstance currentEffect = player.getEffect(CBBEffects.METH.get());
+                int newLevel = currentEffect != null ?
+                        Math.min(currentEffect.getAmplifier() + 1, MAX_LEVEL) : 0;
+                int newDuration = currentEffect != null ?
+                        currentEffect.getDuration() + 1200 : 2400;
+
+                player.addEffect(new MobEffectInstance(CBBEffects.METH.get(), newDuration, newLevel));
+            }
+            return super.finishUsingItem(stack, level, entity);
         }
     }
     public static class White extends MethItem {
+        private static final int MAX_LEVEL = 1;
+
         public White(Properties pProperties) {
-            super(pProperties);
+            super(pProperties.food(new FoodProperties.Builder()
+                    .alwaysEat()
+                    .nutrition(0)
+                    .saturationMod(0)
+                    .effect(() -> new MobEffectInstance(CBBEffects.METH.get(), 600, 0), 1)
+                    .build()));
+        }
+        @Override
+        public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
+            if(!level.isClientSide && entity instanceof ServerPlayer player) {
+                MobEffectInstance currentEffect = player.getEffect(CBBEffects.METH.get());
+                int newLevel = currentEffect != null ?
+                        Math.min(currentEffect.getAmplifier() + 1, MAX_LEVEL) : 0;
+                int newDuration = currentEffect != null ?
+                        currentEffect.getDuration() + 600 : 1200;
+
+                player.addEffect(new MobEffectInstance(CBBEffects.METH.get(), newDuration, newLevel));
+            }
+            return super.finishUsingItem(stack, level, entity);
         }
     }
 }
